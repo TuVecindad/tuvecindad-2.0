9a1350eefce3ac1d6d8ffc158616b44a2634010d
José Alberto <jmartinm@cifpfbmoll.eu>
2020-05-04 19:59:40 +0200

    mensaje de verificación y reseteo funcionando

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Estas credenciales no concuerdan con los datos que tenemos',
+    'throttle' => 'Demasiados intentos. Por favor, inténtalo de nuevo en :seconds segundos.',
 
 ];
