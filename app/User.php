8c0ffe537f1799e63e95325b40e3b4a164cc887f
José Alberto <jmartinm@cifpfbmoll.eu>
2020-05-06 17:48:33 +0200

    probando acceso a sólo usuarios verificados

--- app/User.php
+++ app/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable implements MustVerifyEmail
         'email_verified_at' => 'datetime',
     ];
 
+    public function verifyUser()
+    {
+    return $this->hasOne('App\VerifyUser');
+    }
+
 }
