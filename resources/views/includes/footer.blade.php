335f40d74aad9fb45ca8bc4969e0c5f2d2a29d4e
nesy-gm <44878233+nesy-gm@users.noreply.github.com>
2020-06-07 02:49:12 +0200

    imagen fluida

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,3 @@
-<div class="text-center col-md-12">
-    <p class="m-auto py-2">{{ config('app.name') }} ©2020</p>
+<div class="text-center col-md-12 ">
+    <p class="m-auto py-2 text-white">{{ config('app.name') }} ©2020</p>
 </div>
